75fbad07ed6a05d746715e20452abeb5f29bb3f8
alex <alex7chiaster@gmail.com>
2020-03-15 22:56:32 +0800

    able to connect to POST req from front end

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); // Cross Origin Resource Sharing (CORS)
 const mysql = require("mysql");
 
 const app = express();
@@ -19,7 +19,10 @@ connection.connect(err => {
   }
 });
 
+// Allows the client (broswer) to connect with more than one server at a time e.g. Server A is the webpage but makes an ajax call to Server B to request for data. It is a security mechanism for browsers only. CORS can also give specific access to other
 app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Go to /products to see products");
@@ -37,17 +40,50 @@ app.get("/products", (req, res) => {
   });
 });
 
-app.get("/products/add", (req, res) => {
-  const { name, price } = req.query;
-  const INSERT_PRODUCTS_QUERY = `INSERT INTO products (name, price) VALUES ("${name}", ${price})`;
+// app.get("/products/add", (req, res) => {
+//   const { name, price } = req.query;
+//   const INSERT_PRODUCTS_QUERY = `INSERT INTO products (name, price) VALUES ("${name}", ${price})`;
 
-  connection.query(INSERT_PRODUCTS_QUERY, (err, results) => {
-    if (err) {
-      return res.send(err);
-    } else {
-      return res.send("Successfully added products!");
-    }
-  });
+//   // let data = {
+//   //   name: req.body.name,
+//   //   price: req.body.price
+//   // };
+
+//   console.log(req.query.name);
+
+//   connection.query(INSERT_PRODUCTS_QUERY, (err, results) => {
+//     if (err) {
+//       return res.send(err);
+//     } else {
+//       return res.send("Successfully added products!");
+//     }
+//   });
+//   // res.send("adding product");
+// });
+
+app.post("/products/add", (req, res) => {
+  // const { name, price } = req.body;
+  // const data = {
+  //   name: req.body.name,
+  //   price: req.body.price
+  // };
+
+  // console.log(data.name);
+  // console.log(data.price);
+
+  console.log(req.body);
+
+  // const INSERT_PRODUCTS_QUERY = `INSERT INTO products (name, price) VALUES ("${name}", ${price})`;
+
+  // console.log(req.query.name);
+
+  // connection.query(INSERT_PRODUCTS_QUERY, (err, results) => {
+  //   if (err) {
+  //     return res.send(err);
+  //   } else {
+  //     return res.send("Successfully added products!");
+  //   }
+  // });
   // res.send("adding product");
 });
 
